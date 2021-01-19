45a14588a6862888ba7186f6977529040f4b0bd5
juan <juan_forever@yahoo.es>
2021-01-18 23:22:29 -0600

    primer commit

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+//=================================
+//             puerto
+//=================================
+//no requiere exportarlo, lo hace automaticamente porla variable global
+process.env.PORT = process.env.PORT || 3000;
